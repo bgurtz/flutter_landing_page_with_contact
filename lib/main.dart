f7fc243be15c1b9c7d5774312c76cf3dc57f3dbc
bgurtz <bgurtz@gmail.com>
2019-10-19 23:38:45 -0500

    "Added text, divider, font styling"

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,37 @@ const Home({Key key}) : super(key: key);
       backgroundColor: Colors.amber[200],
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             //Image.asset("assets/images/bg.jpg"),
             CircleAvatar(
+              radius: 55,
               backgroundImage: AssetImage("assets/images/bg.jpg"),
-            )
+            ),
+            Text("Brian Gurtz",
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                //fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+            Text("Softwear Engineer",
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.amber[50],
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Source Sans Pro',
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: 200,
+              child: Divider(
+                color: Colors.amber[700],
+              ),
+            ),
           ],
         ),
       ),
